2ec2011f43be5b9d82f2e0cfea8b0c22f9f07a2a
J Aguilar <jaguil24@my.centennialcollege.ca>
2025-01-22 05:57:33 -0500

    Update greeting to display 'Hello [My Name]' and added .gitignore

--- MyCoreApp/obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
+++ MyCoreApp/obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3146d416095f6598dcb3b49c170154e0dc9cc4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
